ccd990cc46f197b2acf28ccb0368ca54e96c9e02
shifachaus <chausshifa804@gmail.com>
2023-11-02 18:08:06 +0530

    Added character count feature for live chat

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -12,7 +12,7 @@ const Chat = () => {
 
   return (
     <form
-      className={` w-full mx-auto px-3 py-3 mt-2`}
+      className={` w-full mx-auto px-3 py-3 `}
       onSubmit={(e) => {
         e.preventDefault();
         dispatch(
@@ -25,29 +25,56 @@ const Chat = () => {
       }}
     >
       <div className="flex flex-col  gap-2 ml-3">
-        {myUser && (
-          <div className="flex items-center  gap-2">
+        <div className="flex items-center  gap-2">
+          {myUser && (
             <img
               className="w-8 h-8 rounded-full"
               src={myUser?.picture}
               alt="avatar"
             />
-            <p className="text-sm text-neutral-400 font-medium">
-              {myUser?.nickname || myUser?.given_name}
-            </p>
-          </div>
-        )}
+          )}
+          <p className="text-sm text-neutral-200 font-medium">
+            {myUser?.nickname || myUser?.given_name}
+          </p>
+        </div>
+
         <div className="flex items-end flex-col ml-8 gap-2">
           <input
-            className="text-sm font-medium text-neutral-400 placeholder:text-neutral-400 px-2 w-full bg-black border-b border-neutral-400"
+            className="text-sm font-medium text-neutral-200 placeholder:text-neutral-200 px-2 w-full bg-black border-b border-neutral-400"
             placeholder="Chat..."
             type="text"
             value={liveMessage}
             onChange={(e) => setLiveMessage(e.target.value)}
           />
-          <span className=" mx-2">
-            <VscSend className="text-2xl text-neutral-400 " />
-          </span>
+
+          <div className="flex items-center">
+            <p
+              className={
+                liveMessage.length > 150 && liveMessage.length <= 180
+                  ? "text-[.8rem] text-yellow-500"
+                  : liveMessage.length > 180 && liveMessage.length <= 200
+                  ? "text-[.8rem] text-orange-500"
+                  : liveMessage.length > 200
+                  ? "text-[.8rem] text-red-600"
+                  : "text-[.8rem] text-neutral-400"
+              }
+            >
+              {liveMessage.length}/200
+            </p>
+            <button
+              disabled={liveMessage.length === 0}
+              type="submit"
+              className=" mx-2"
+            >
+              <VscSend
+                className={
+                  liveMessage.length > 0 && liveMessage.length <= 200
+                    ? "text-2xl text-neutral-400"
+                    : `text-2xl text-neutral-600 `
+                }
+              />
+            </button>
+          </div>
         </div>
       </div>
     </form>
